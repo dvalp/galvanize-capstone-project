38ef23147e89a774c908840798580f0cd5678cad
David Valpey <greengargoyle@hotmail.com>
2016-11-16 13:43:32 -0800

    added more memory to spark, word2vec was filling it up

--- scripts/ipython-spark.sh
+++ scripts/ipython-spark.sh
@@ -4,8 +4,8 @@ export PYSPARK_DRIVER_PYTHON=ipython
 
 /usr/local/spark/bin/pyspark \
 	--master local[6] \
-	--executor-memory 12G \
-	--driver-memory 12G \
+	--executor-memory 20G \
+	--driver-memory 20G \
 	--packages com.databricks:spark-csv_2.11:1.5.0 \
         --packages com.amazonaws:aws-java-sdk-pom:1.10.34 \
         --packages org.apache.hadoop:hadoop-aws:2.7.3
