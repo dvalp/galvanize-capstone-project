6843426c17687e04d2cac54f9ab87fac13e0f0ab
David Valpey <greengargoyle@hotmail.com>
2016-11-16 15:53:08 -0800

    added more vectors to word2vec, changed minDF to 10 for TF and IDF

--- src/spark.py
+++ src/spark.py
@@ -20,7 +20,7 @@ reduced_rdd = opinion_rdd.map(lambda row: [row.get(key, '') for key in df_column
 opinion_df = spark.createDataFrame(reduced_rdd, schema)
 
 # create a test dataframe
-test_parse = sc.parallelize(opinion_rdd.take(10), 15)
+test_parse = sc.parallelize(opinion_rdd.take(100), 15)
 test_rdd = test_parse.map(lambda row: [row.get(key,'') for key in df_columns])
 test_df = spark.createDataFrame(test_rdd, schema)
 
@@ -43,17 +43,20 @@ op_tokens_df = bigram.transform(op_tokens_df)
 op_tokens_df = trigram.transform(op_tokens_df)
 
 # CountVectorizer
-cv = CountVectorizer(inputCol='tokens_stop', outputCol='token_countvector', minDF=2.0)
+cv = CountVectorizer(inputCol='tokens_stop', outputCol='token_countvector', minDF=10.0)
 opinion_cv_model = cv.fit(op_tokens_df)
 op_tokens_df = opinion_cv_model.transform(op_tokens_df)
 
 # IDF
-idf = IDF(inputCol='token_countvector', outputCol='token_idf', minDocFreq=2)
+idf = IDF(inputCol='token_countvector', outputCol='token_idf', minDocFreq=10)
 opinion_idf_model = idf.fit(op_tokens_df)
 op_tokens_df = opinion_idf_model.transform(op_tokens_df)
 
 # Word2Vec
-w2v = Word2Vec(vectorSize=3, minCount=2, inputCol='tokens_stop', outputCol='token_word2vec')
-opinion_w2v_model = w2v.fit(op_tokens_df)
-op_tokens_df = opinion_w2v_model.transform(op_tokens_df)
+w2v_2d = Word2Vec(vectorSize=2, minCount=2, inputCol='tokens_stop', outputCol='token_word2vec_2d')
+w2v_large = Word2Vec(vectorSize=250, minCount=2, inputCol='tokens_stop', outputCol='token_word2vec_large')
+opinion_w2v2d_model = w2v_2d.fit(op_tokens_df)
+opinion_w2vlarge_model = w2v_large.fit(op_tokens_df)
+op_tokens_df = opinion_w2v2d_model.transform(op_tokens_df)
+op_tokens_df = opinion_w2vlarge_model.transform(op_tokens_df)
 
