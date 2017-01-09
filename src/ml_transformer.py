84d776eee05b657d164beecae9654e4dfa2d12a2
David Valpey <greengargoyle@hotmail.com>
2017-01-09 02:29:37 -0800

    added a little more to the transformer

--- src/ml_transformer.py
+++ src/ml_transformer.py
@@ -1,6 +1,9 @@
+from pyspark.ml.util import keyword_only
 from pyspark.ml.pipeline import Transformer
 from pyspark.ml.param.shared import HasInputCol, HasOutputCol
 
 # Custom stemming transformer class for pyspark
 class stemming_transformer(Transformer, HasInputCol, HasOutputCol):
-    pass
+    @keyword_only
+    def __init__(self, inputCol=None, outputCol=None):
+        pass
