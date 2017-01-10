ab1fbdbd8fdd6d33b919cead46346e4f21fdc495
David Valpey <greengargoyle@hotmail.com>
2017-01-10 15:46:45 -0800

    more outline for the stemming transformer

--- src/ml_transformer.py
+++ src/ml_transformer.py
@@ -1,6 +1,7 @@
 from pyspark.ml.util import keyword_only
 from pyspark.ml.pipeline import Transformer
 from pyspark.ml.param.shared import HasInputCol, HasOutputCol
+from nltk.stem import SnowballStemmer
 
 # Custom stemming transformer class for pyspark
 class stemming_transformer(Transformer, HasInputCol, HasOutputCol):
@@ -8,5 +9,9 @@ class stemming_transformer(Transformer, HasInputCol, HasOutputCol):
     def __init__(self, inputCol=None, outputCol=None):
         pass
 
-    def _transform():
+    @keyword_only
+    def setParam(self, inputCol=None, outputCol=None):
+        pass
+
+    def _transform(self, dataset):
         pass
